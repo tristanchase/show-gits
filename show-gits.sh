3d9975c3ce0b527106709e3c99536a1b56869882
Tristan Chase <tristan.m.chase@gmail.com>
2020-08-24 23:35:16 -0400

    Add colors to output
    I created a dotfile called "colors.sh" that has all the terminal escape
    sequences defined as variables which can then be sourced by scripts.

--- show-gits.sh
+++ show-gits.sh
@@ -93,9 +93,12 @@ if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
 #-----------------------------------
 # Main Script goes here
 
+if [[ -e ~/.colors.sh ]]; then
+	source ~/.colors.sh
+fi
 
 # Create temp file for output of find
-# TODO Use variable only?
+# TODO Use array only?
 _dirfile="${HOME}"/tmp/show-gits.$$.tempfile
 touch ${_dirfile}
 
@@ -105,14 +108,15 @@ _startdir="$(pwd)"
 # Find the git repos in the ${HOME} directory (but exclude ~/.cache/)
 printf "%b\n" ${HOME}/**/.git | sed 's/\/\.git//g' > ${_dirfile}
 printf '%b\n' ${HOME}/.*/**/.git | grep -Ev '/\.(\.|cache)?/' | sed 's/\/\.git//g' >> ${_dirfile}
-#printf '%b\n' ${HOME}/.*/**/.git | grep -Ev '/\.(\.)?/' | sed 's/\/\.git//g' >> ${_dirfile}
 
 # Find files with trailing whitespace (but not .pdf's or other binary files)
 function __find_trailing_whitespace(){
 	if [[ -n "$(grep --files-with-matches --binary-files=without-match '\s$' 2>/dev/null "${_dir}"/*)" ]]; then
-		printf "%b\n" ">>>These files have trailing whitespace:"
+
+		printf "${WHT:-}${CYNB:-}%s\n" ">>>These files have trailing whitespace:"
 		grep  --files-with-matches --binary-files=without-match '\s$' 2>/dev/null "${_dir}"/* | xargs realpath
-		printf '%b\n'
+
+		printf ""${reset:-}"%b\n"
 	fi
 }
 
@@ -128,7 +132,7 @@ function __git_prompt {
 function __get_list_short(){
 	for _dir in $(cat ${_dirfile}); do
 		cd "${_dir}"
-		printf "%b\n" "${_dir}$(__git_prompt)"
+		printf ""${BBLU:-}"%s"${BCYN:-}"%s\n"${reset:-}"" "${_dir}" "$(__git_prompt)"
 		git -C "${_dir}" status -s
 		__find_trailing_whitespace
 	done
@@ -136,7 +140,8 @@ function __get_list_short(){
 
 # Show the repos (-l|--list)
 function __show_repos(){
-	cat ${_dirfile}
+	#cat ${_dirfile}
+	printf ""${BBLU:-}"%s\n"${reset:-}"" "$(cat ${_dirfile})"
 }
 
 # Update the repos from remote (-u|--update)
@@ -152,7 +157,8 @@ function __get_full_status(){
 	for _dir in $(cat ${_dirfile}); do
 		#printf "%b\n" ${_dir}
 		cd "${_dir}"
-		printf "%b\n" "${_dir}$(__git_prompt)"
+		#printf "%b\n" "${_dir}$(__git_prompt)"
+		printf ""${BBLU:-}"%s"${BCYN:-}"%s\n"${reset:-}"" "${_dir}" "$(__git_prompt)"
 		git -C "${_dir}" status
 		__find_trailing_whitespace
 		printf "%b\n" ""
@@ -165,7 +171,8 @@ function __get_short_status() {
 		if [[ -n "$(git -C "${_dir}" status -s)" ]]; then
 			#printf "%b\n" ${_dir}
 			cd "${_dir}"
-			printf "%b\n" "${_dir}$(__git_prompt)"
+			#printf "%b\n" "${_dir}$(__git_prompt)"
+			printf ""${BBLU:-}"%s"${BCYN:-}"%s\n"${reset:-}"" "${_dir}" "$(__git_prompt)"
 			git -C "${_dir}" status -s
 			__find_trailing_whitespace
 		fi
