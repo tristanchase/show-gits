74358fd9cfdba8b9ca8fc49e3ff36cc7b3441374
Tristan Chase <tristan.m.chase@gmail.com>
2020-11-22 10:41:55 -0500

    Options not working; change layout
    Completely reworked the layout within the new template. Options are
    working again.

--- show-gits.sh
+++ show-gits.sh
@@ -20,20 +20,12 @@
 
 #-----------------------------------
 # TODO Section
-
-# * Options not working; change layout
+# * Add warning if sourced files are missing
+# * Replace old color variables with new ones
+# * Replace _dirfile tempfile with array
 
 # DONE
-# + Insert script
-# + Clean up indentation
-# + Clean up stray ;'s
-# + Modify command substitution to "$(this_style)"
-# + Rename function_name() to function __function_name__ /\w+\(\)
-# + Remove redundant functions sourced from ~/.functions.sh
-# + Rename $variables to "${_variables}" /\$\w+/s+1 @v (vEl,{n)
-# + Check that _variable="variable definition" (make sure it's in quotes)
-# + Update usage, description, and options section
-# + Update dependencies section
+# + Options not working; change layout
 
 #-----------------------------------
 # License Section
@@ -64,83 +56,15 @@ function __main_script__ {
 	printf "%b\n" "${HOME}"/**/.git | sed 's/\/\.git//g' > "${_dirfile}"
 	printf '%b\n' "${HOME}"/.*/**/.git | grep -Ev '/\.(\.|cache)?/' | sed 's/\/\.git//g' >> "${_dirfile}"
 
-	# Find files with trailing whitespace (but not .pdf's or other binary files)
-	# Sourced from ~/.functions.sh
-#	function __find_trailing_whitespace__{
-#		if [[ -n "$(grep --files-with-matches --binary-files=without-match '\s$' 2>/dev/null "${_dir}"/*)" ]]; then
-#			printf "${WHT:-}${CYNB:-}%s\n" ">>>These files have trailing whitespace:"
-#			grep --files-with-matches --binary-files=without-match '\s$' 2>/dev/null "${_dir}"/* | xargs realpath
-#			printf ""${reset:-}"%b\n"
-#		fi
-#	}
-
-	# Show git status à la git-prompt.sh
-	function __git_prompt__ {
-		if [[ -e "${HOME}"/.git-prompt.sh ]]; then
-			source ~/.git-prompt.sh
-			__git_ps1__ 2>/dev/null
-		fi
-	}
-
-	# Get a list of the repos with the short status (default)
-	function __get_list_short__ {
-		for _dir in $(cat "${_dirfile}"); do
-			cd "${_dir}"
-			printf ""${BBLU:-}"%s"${BCYN:-}"%s\n"${reset:-}"" "${_dir}" "$(__git_prompt__)"
-			git -C "${_dir}" status -s
-			__find_trailing_whitespace__
-		done
-	}
-
-	# Show the repos (-l|--list)
-	function __show_repos__ {
-		#cat "${_dirfile}"
-		printf ""${BBLU:-}"%s\n"${reset:-}"" "$(cat "${_dirfile}")"
-	}
-
-	# Update the repos from remote (-u|--update)
-	function __fetch_remotes__ {
-		for _dir in $(cat "${_dirfile}"); do
-			printf "%b\n" "${_dir}"
-			git -C "${_dir}" remote update
-		done
-	}
-
-	# Get the full status of the repos (-f|--full)
-	function __get_full_status__ {
-		for _dir in $(cat "${_dirfile}"); do
-			#printf "%b\n" "${_dir}"
-			cd "${_dir}"
-			#printf "%b\n" ""${_dir}"$(__git_prompt__)"
-			printf ""${BBLU:-}"%s"${BCYN:-}"%s\n"${reset:-}"" "${_dir}" "$(__git_prompt__)"
-			git -C "${_dir}" status
-			__find_trailing_whitespace__
-			printf "%b\n" ""
-		done
-	}
-
-	# Get the short status of the repos (-s|--status)
-	function __get_short_status__ {
-		for _dir in $(cat "${_dirfile}"); do
-			if [[ -n "$(git -C "${_dir}" status -s)" ]]; then
-				#printf "%b\n" "${_dir}"
-				cd "${_dir}"
-				#printf "%b\n" ""${_dir}"$(__git_prompt__)"
-				printf ""${BBLU:-}"%s"${BCYN:-}"%s\n"${reset:-}"" "${_dir}" "$(__git_prompt__)"
-				git -C "${_dir}" status -s
-				__find_trailing_whitespace__
-			fi
-		done
-	}
 
 	# Runtime
-	if [[ "${1:-}" =~ (-u|--update) ]]; then
+	if [[ "${_fetch_remotes_yN:-}" = "y" ]];then
 		__fetch_remotes__
-	elif [[ "${1:-}" =~ (-s|--status) ]]; then
+	elif [[ "${_get_short_status_yN:-}" = "y" ]];then
 		__get_short_status__
-	elif [[ "${1:-}" =~ (-l|--list) ]]; then
+	elif [[ "${_show_repos__yN:-}" = "y" ]];then
 		__show_repos__ | more
-	elif [[ "${1:-}" =~ (-f|--full) ]]; then
+	elif [[ "${_get_full_status__yN:-}" = "y" ]];then
 		__get_full_status__ | more
 	else
 		__get_list_short__ | more
@@ -153,6 +77,74 @@ function __main_script__ {
 } #end __main_script__
 
 # Local functions
+
+# Find files with trailing whitespace (but not .pdf's or other binary files)
+function __find_trailing_whitespace_l__ {
+	if [[ -n "$(grep --files-with-matches --binary-files=without-match '\s$' 2>/dev/null "${_dir}"/*)" ]]; then
+		printf "${WHT:-}${CYNB:-}%s\n" ">>>These files have trailing whitespace:"
+		grep --files-with-matches --binary-files=without-match '\s$' 2>/dev/null "${_dir}"/* | xargs realpath
+		printf ""${reset:-}"%b\n"
+	fi
+}
+
+# Show git status à la git-prompt.sh
+function __git_prompt__ {
+	if [[ -e "${HOME}"/.git-prompt.sh ]]; then
+		source ~/.git-prompt.sh
+		__git_ps1 2>/dev/null
+	fi
+}
+
+# Get a list of the repos with the short status (default)
+function __get_list_short__ {
+	for _dir in $(cat "${_dirfile}"); do
+		cd "${_dir}"
+		printf ""${BBLU:-}"%s"${BCYN:-}"%s\n"${reset:-}"" "${_dir}" "$(__git_prompt__)"
+		git -C "${_dir}" status -s
+		__find_trailing_whitespace_l__
+	done
+}
+
+# Show the repos (-l|--list)
+function __show_repos__ {
+	#cat "${_dirfile}"
+	printf ""${BBLU:-}"%s\n"${reset:-}"" "$(cat "${_dirfile}")"
+}
+
+# Update the repos from remote (-u|--update)
+function __fetch_remotes__ {
+	for _dir in $(cat "${_dirfile}"); do
+		printf "%b\n" "${_dir}"
+		git -C "${_dir}" remote update
+	done
+}
+
+# Get the full status of the repos (-f|--full)
+function __get_full_status__ {
+	for _dir in $(cat "${_dirfile}"); do
+		#printf "%b\n" "${_dir}"
+		cd "${_dir}"
+		#printf "%b\n" ""${_dir}"$(__git_prompt__)"
+		printf ""${BBLU:-}"%s"${BCYN:-}"%s\n"${reset:-}"" "${_dir}" "$(__git_prompt__)"
+		git -C "${_dir}" status
+		__find_trailing_whitespace_l__
+		printf "%b\n" ""
+	done
+}
+
+# Get the short status of the repos (-s|--status)
+function __get_short_status__ {
+	for _dir in $(cat "${_dirfile}"); do
+		if [[ -n "$(git -C "${_dir}" status -s)" ]]; then
+			#printf "%b\n" "${_dir}"
+			cd "${_dir}"
+			#printf "%b\n" ""${_dir}"$(__git_prompt__)"
+			printf ""${BBLU:-}"%s"${BCYN:-}"%s\n"${reset:-}"" "${_dir}" "$(__git_prompt__)"
+			git -C "${_dir}" status -s
+			__find_trailing_whitespace_l__
+		fi
+	done
+}
 
 function __local_cleanup__ {
 	:
@@ -169,6 +161,14 @@ if [[ "${1:-}" =~ (-d|--debug) ]]; then
 	__debugger__
 elif [[ "${1:-}" =~ (-h|--help) ]]; then
 	__usage__
+elif [[ "${1:-}" =~ (-u|--update) ]]; then
+	_fetch_remotes_yN="y"
+elif [[ "${1:-}" =~ (-s|--status) ]]; then
+	_get_short_status_yN="y"
+elif [[ "${1:-}" =~ (-l|--list) ]]; then
+	_show_repos__yN="y"
+elif [[ "${1:-}" =~ (-f|--full) ]]; then
+	_get_full_status__yN="y"
 fi
 
 # Bash settings
